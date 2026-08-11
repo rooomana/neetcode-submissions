c0936e84102b7397e88bf6a15c2cccc47909cfe7
Miguel Romana <mamra2@iscte-iul.pt>
2026-08-11 19:41:40 +0100

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = "".join(c.lower() for c in s if c.isalnum())
+#        odd_check = len(s) % 2
+#        middle = len(s) // 2
+#        return s[:middle] == s[:middle - 1 + odd_check:-1]
+        return s == s[::-1]
